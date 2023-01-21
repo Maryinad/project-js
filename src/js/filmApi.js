fbc6d05d867bf0d09b51cf2f0037ff9aa2180f34
Firsova Liubov <firsova.lyub@gmail.com>
2023-01-21 16:37:47 +0200

    added class FilmAPI with methods

---
+++ src/js/filmApi.js
@@ -0,0 +1,53 @@
+'use strict';
+
+import axios from "axios";
+
+export class FilmAPI {
+    static BASE_URL = 'http://api.themoviedb.org/3/'; 
+    static API_KEY = 'a1c58729dd9d07a228419b3d2b553cbb';
+
+    constructor() {
+        this.page = 1;
+        this.query = null;
+        // this._makeGenresObject();
+    }
+    // _makeGenresObject() {
+    //     this.fetchGenres().then(({ data }) => {
+    //         const genresInfo = data.genres;
+    //         genresList = data.genres;
+    //         this.genresObject = genresInfo.reduce((acum, { id, name }) => ({ ...acum, [id]: name }), {});
+    //     })
+    // }
+    async fetchTrendingFilms() {
+        const params = {
+            params:{
+                _page: this.page,
+                api_key: FilmAPI.API_KEY
+            }
+        }
+        const response = await axios.get(`${FilmAPI.BASE_URL}trending/movie/week`, params);
+        return response.data;
+    }
+    async fetchFilmsByQuery() {
+        const params = {
+            params:{
+                _page: this.page,
+                api_key: FilmAPI.API_KEY,
+                query: this.query
+            }
+        }
+        const response = await axios.get(`${FilmAPI.BASE_URL}/search/movie`, params);
+        console.log(response.data);
+        return response.data;
+    }
+    async fetchFilmById(id) {
+        const movie_id = id;
+        const params = {
+            params:{
+                api_key: FilmAPI.API_KEY,
+            }
+        }
+        const response = await axios.get(`${FilmAPI.BASE_URL}movie/${movie_id}`, params);
+        return response;
+    }
+}
---
+++ src/js/gallery.js
@@ -0,0 +1,10 @@
+import { FilmAPI } from "./filmApi";
+import axios from "axios";
+
+const filmAPI = new FilmAPI();
+
+filmAPI.fetchTrendingFilms().then(resp => resp.results);
+
+// filmAPI.fetchFilmsByQuery(query);
+
+// filmAPI.fetchFilmById();
