d4cec1fbaefe559f54e870d47df7d04933706217
Maryna Ilinova <80976268+Maryinad@users.noreply.github.com>
2023-01-23 00:29:16 +0200

    Added

---
+++ src/js/fetch_trending_films.js
@@ -0,0 +1,22 @@
+import { FilmAPI } from './filmApi';
+import { markupFilmCard } from './filmCardMarkUp';
+
+const galleryEl = document.querySelector('.card-library');
+const filmApi = new FilmAPI();
+
+// const data = filmApi.fetchTrendingFilms().then(data => {
+//   galleryEl.insertAdjacentHTML('beforeend', markupFilmCard(data.results));
+// });
+
+// async function fetchTrendingFilms() {
+//   try {
+//     const data = await filmApi.fetchTrendingFilms();
+//     console.log(data.results);
+
+//     galleryEl.insertAdjacentHTML('beforeend', markupFilmCard(data.results));
+//   } catch {
+//     err => console.log(err);
+//   }
+// }
+// fetchTrendingFilms();
+// где ее вызывать? нужно ли этот файл добавлять в index.js
--- src/js/refs.js
+++ src/js/refs.js
@@ -1,7 +1,11 @@
-const refs = {
+export const refs = {
   inputEl: document.querySelector('.js-header__input'),
   formEl: document.querySelector('.js-header__form'),
   btnSearchEl: document.querySelector('.js-btn_search'),
   btnHeaderWatchedEl: document.querySelector('.js-header__btn-watched'),
   btnHeaderQueueEl: document.querySelector('.js-header__btn-queue'),
+  galleryCardLibraryEl: document.querySelector('.js-card-library'),
+  galleryItemEl: document.querySelector('.js-card-library__item'),
+  footerLinkEl: document.querySelector('.js-footer__link'),
+  footerIconEl: document.querySelector('.js-footer__icon'),
 };
