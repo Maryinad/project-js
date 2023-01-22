489f4116cc5028a875888fe03f133683594d8c6f
Maryna Ilinova <80976268+Maryinad@users.noreply.github.com>
2023-01-22 13:35:19 +0200

    added

---
+++ src/js/refs.js
@@ -0,0 +1,7 @@
+const refs = {
+  inputEl: document.querySelector('.js-header__input'),
+  formEl: document.querySelector('.js-header__form'),
+  btnSearchEl: document.querySelector('.js-btn_search'),
+  btnHeaderWatchedEl: document.querySelector('.js-header__btn-watched'),
+  btnHeaderQueueEl: document.querySelector('.js-header__btn-queue'),
+};
