c49170960ba5d5040bf14eaeefe182c41fd55311
Maryna Ilinova <80976268+Maryinad@users.noreply.github.com>
2023-01-23 01:35:35 +0200

    added

--- src/js/refs.js
+++ src/js/refs.js
@@ -1,6 +1,6 @@
 export const refs = {
   inputEl: document.querySelector('.js-header__input'),
-  formEl: document.querySelector('.js-header__form'),
+  //   formEl: document.querySelector('.js-header__form'),
   btnSearchEl: document.querySelector('.js-btn_search'),
   btnHeaderWatchedEl: document.querySelector('.js-header__btn-watched'),
   btnHeaderQueueEl: document.querySelector('.js-header__btn-queue'),
