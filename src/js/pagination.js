dd5138e847fff65567aa3d2c3b9ccf5fd1b91ce8
OlesiaMelnyk1 <elsonidodelmar26@gmail.com>
2023-01-24 01:30:59 +0200

    add pagination-new

---
+++ src/js/pagination.js
@@ -0,0 +1,45 @@
+import Pagination from 'tui-pagination';
+// import 'tui-pagination/dist/tui-pagination.css';
+import { FilmAPI } from './filmApi';
+const container = document.querySelector('#pagination');
+
+const filmApi = new FilmAPI();
+
+const options = {
+  totalItems: 100,
+  itemsPerPage: 20,
+  visiblePages: 5,
+  page: 1,
+  centerAlign: true,
+  firstItemClassName: 'tui-first-child',
+  lastItemClassName: 'tui-last-child',
+  template: {
+    page: '<a href="#" class="tui-page-btn">{{page}}</a>',
+    currentPage:
+      '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
+    moveButton:
+      '<a href="#" class="tui-page-btn tui-{{type}}">' +
+      '<span class="tui-ico-{{type}}">{{type}}</span>' +
+      '</a>',
+    disabledMoveButton:
+      '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
+      '<span class="tui-ico-{{type}}">{{type}}</span>' +
+      '</span>',
+    moreButton:
+      '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
+      '<span class="tui-ico-ellip">...</span>' +
+      '</a>',
+  },
+};
+const pagination = new Pagination(container, options);
+
+pagination.on('afterMove', e => {
+  console.log(e.page);
+  filmApi.page = e.page;
+});
+
+// export function paginationStart(data) {
+//   options.totalItems = data.results;
+//   pagination = new Pagination(container, options);
+//   return pagination;
+// }
