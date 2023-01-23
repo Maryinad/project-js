cded53caaeab66866350918b7ebe49dab219d01f
Tishko1 <113176690+Tishko1@users.noreply.github.com>
2023-01-23 12:00:48 +0300

    add markup

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,9 +1,9 @@
 import Notiflix from 'notiflix';
-//   import { MovieDB } from './api-service';
+import { FilmAPI } from './filmApi';
 // import { numberConverter } from './prepare-number';
 // import * as basicLightbox from 'basiclightbox';
 // import 'basiclightbox/dist/basicLightbox.min.css'
-// import defaultPhoto from '../images/default-photo.jpeg';
+import defaultPhoto from '../images/default-photo.jpeg';
 const modalOpenEl = document.querySelector('[data-modal-open]');
 const modalCloseEl = document.querySelector('[data-modal-close]');
 const modalEl = document.querySelector('[data-modal]');
@@ -22,7 +22,7 @@ const QUEUE_STORAGE_KEY = 'films in queue';
 
 modalOpenEl.addEventListener('click', onModalOpenClick);
 
-// const movieDB = new MovieDB();
+const filmAPI = new FilmAPI();
 
 // ======================================================
 // Функції-обробники закриття/відериття модального вікна
@@ -48,23 +48,64 @@ function onModalCloseClick() {
   window.removeEventListener('keydown', onEscBtnClick);
 }
 // Головна функція-обробник появи модального вікна
-function onModalOpenClick(event) {
+async function onModalOpenClick(event) {
   event.preventDefault();
   if (event.target.closest('li')) {
-    Notiflix.Loading.pulse({
-      backgroundColor: 'rgba(0,0,0,0.8)',
-      svgColor: '#ff6b08',
-    });
-
+    
     modalEl.classList.remove('is-hidden');
     modalCloseEl.addEventListener('click', onModalCloseClick);
     backdropEl.addEventListener('click', onBackdropElClick);
     window.addEventListener('keydown', onEscBtnClick);
 
+    const selectedFilm = event.target.closest('li');
+    console.log(selectedFilm);
+    const FilmID = selectedFilm.dataset.movieid;
+
+
+    
+    Notiflix.Loading.pulse({
+      backgroundColor: 'rgba(0,0,0,0.8)',
+      svgColor: '#ff6b08',
+    });
+
+    // &&&&&&&&&&&?
+    const { data } = await filmAPI.fetchFilmById(FilmID);
+    renderFilmCard(data);
+
     Notiflix.Loading.remove(3023);
 
-    const selectedMovie = event.target.closest('li');
-    console.log(selectedMovie);
-    const FilmID = selectedMovie.dataset.movieid;
   }
 }
+
+function prepareObject(array) {
+  let newArr = array.map(el => el.name);
+  let filmGenres = '';
+
+  if (newArr.length < 4) {
+    filmGenres = newArr.join(', ');
+  }
+  if (newArr.length >= 4) {
+    filmGenres = newArr.slice(0, 2).join(', ') + ', Other';
+  }
+  return filmGenres;
+}
+
+function numberConverter(number) {
+  let numRound = null;
+  let numString = '';
+
+  // Якщо рейтинг від 0 до 10 не включно
+  // але цілі числа
+  if (number.toString().length === 1) {
+    numString = String(number) + '.0';
+    return numString;
+  }
+
+  // Якщо рейтинг від 0 до 10 не включно
+  // але не цілі числа
+  if (number / 10 < 1) {
+    numRound = (Math.round(number * 10) / 10).toString();
+    if (numRound.length === 1) {
+      numString = String(numRound) + '.0';
+      return numString;
+    }
